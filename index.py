2ea5bca0d841c5f758ab6449b3324a568dfe9d60
xiaojie zhu <xiaojiez@ifi.uio.no>
2020-11-08 18:28:59 +0100

    index and search
    build index and execute search

---
+++ index.py
@@ -0,0 +1,55 @@
+def higherarchial_index_gen(bfpath, number_of_patients,  patients_snp_matrx, ):
+  r=random.generation()
+  tbf = pybloomfilter.BloomFilter.open(bfpath+"template.bloom")
+  patients={}
+  SNPs={}
+  X = np.zeros([number_of_patients, 20101326], dtype=int)
+  for i in range(number_of_patients):
+    bf = tbf.copy_template(bfpath+str(i)+".bloom")
+
+    SNPs[i]=patients_snp_matrx[i]
+    for j in SNPs[i]:
+      bf.add(j)
+    bf.close()
+
+    tmp = bitarray.bitarray()
+    with open(bfpath+str(i)+".bloom", 'rb') as fh:
+      tmp.fromfile(fh)
+      try:
+        tmp=tmp[-20101326:]
+        idx=tmp.index(1, 0, 20101326)
+        while idx<20101326:
+          alpha=prf(k+str(pos))
+          alpha=prf(alpha+str(r))
+          X[i,alpha]=1
+          idx = tmp.index(1, idx+1, 20101326)
+      except:
+          print("error in index generation ")
+  Z = hierarchy.linkage(X, method='ward', metric="euclidean", optimal_ordering=True )
+  rootnode, nodelist =hierarchy.to_tree(Z,rd=True)
+  return rootnode, nodelist
+
+def search(rootnode, query, threshold,)
+rootnodes={}
+rootnodes.append(rootnode)
+stach={}
+result={}
+for rootnode in rootnodes:
+  stack.append(rootnode)
+  while len(stack)>0:
+    index= len(stack)
+    tempnode = stack.pop(index-1)
+    if not tempnode.is_leaf():
+      left=tempnode.left
+      right=tempnode.right
+      id = left.id
+      score=np.dot(query, patients[id])
+      if score > threshold:
+        stack.append(left)
+      id = right.id
+      score = np.dot(query, patients[id])
+      if score > threshold:
+        stack.append(right)
+    else:
+      result.append(tempnode.id)
+return result 
